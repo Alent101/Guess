19c9f7d938286610bc1c8e22e113faef150a391e
Alent <alent101@gmail.com>
2019-07-09 00:23:20 +0800

    test case

---
+++ app/src/androidTest/java/com/alent/guess/MaterialActivityTest.kt
@@ -0,0 +1,40 @@
+package com.alent.guess
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MaterialActivityTest {
+    //取得秘密數字的前置動作
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule<MaterialActivity>(MaterialActivity::class.java)
+
+    @Test
+    fun guessWrong() {
+        val resources = activityTestRule.activity.resources
+        //取得秘密數字
+        val secret = activityTestRule.activity.secretNumber.secret
+        for (n in 1..10) {
+            if (n != secret) {
+                onView(withId(R.id.ed_number)).perform(clearText())
+                onView(withId(R.id.ed_number)).perform(typeText(n.toString()))
+                onView(withId(R.id.btn_ok)).perform(click())
+                val message =
+                    if (n < secret) resources.getString(R.string.bigger)
+                    else resources.getString(R.string.smaller)
+                onView(withText(message)).check(matches(isDisplayed()))
+                onView(withText(resources.getString(R.string.ok))).perform(click())
+            }
+        }
+
+
+    }
+}
